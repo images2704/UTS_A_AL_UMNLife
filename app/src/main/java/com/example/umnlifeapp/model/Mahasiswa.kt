ae20f20e28d21121ad4ad95a3ee9d39ec61247c3
Stefan Aprilio <45485778+images2704@users.noreply.github.com>
2023-04-04 09:46:45 +0700

    Perubahan 4

--- app/src/main/java/com/example/umnlifeapp/model/Mahasiswa.kt
+++ app/src/main/java/com/example/umnlifeapp/model/Mahasiswa.kt
@@ -4,8 +4,6 @@ class Mahasiswa(val nama:String, private var kenyang: Int = 50,
                 var energi: Int = 50,
                 var motivasi: Int = 50, var pengetahuan: Int = 0)
 {
-
-    //sdsd
     fun tambahiKenyang(angka: Int) {
         kenyang += angka
     }
@@ -18,7 +16,23 @@ class Mahasiswa(val nama:String, private var kenyang: Int = 50,
         pengetahuan += angka
     }
 
+    fun tambahiEnergi(angka: Int) {
+        energi += angka
+    }
+
+    fun kurangiEnergi(angka: Int) {
+        energi -= angka
+    }
+
     fun kurangiPengetahuan(angka: Int) {
         pengetahuan -= angka
+    }
+
+    fun tambahiMotivasi(angka: Int) {
+        motivasi += angka
+    }
+
+    fun kurangiMotivasi(angka: Int) {
+        motivasi -= angka
     }
 }
